f7f77b1c8bb966c683db9361be0b48635b53bf8b
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2024-03-15 12:31:48 +0100

    fix wrap and start item count

--- algo.c
+++ algo.c
@@ -5,21 +5,24 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <string.h>
 #include <math.h>
 
 #define FPS 60
-#define WIDTH 900
+#define WIDTH 1200
 #define HEIGHT 600
 #define SIZE 200
 
 
 typedef struct Box {
-    int value;
+    char value[1024];
     int number;
     struct Box *next;
+    int posX;
+    int posY;
 } Box;
 
 
@@ -31,7 +34,7 @@ bool isInsideRect(int x, int y, SDL_Rect rect) {
 
 int index = 0;
 
-Box *head = NULL;
+Box *head[] = {NULL, NULL, NULL, NULL, NULL, NULL};
 
 int gap = 5, height_of_element = 50;
 int espace(int number) {return height_of_element * number + gap * ++number;};
@@ -43,13 +46,16 @@ void create_box (SDL_Renderer *rend, int x, int y) {
     SDL_RenderFillRect (rend, &box);
 }
 
-void create_element_of_box (){
+void create_element_of_box (int boite, char nom[]){
     Box *newBox = malloc(sizeof(Box));
-    newBox -> value = ++index, newBox -> next = NULL;
-    if(head == NULL) {
-        head = newBox;
+    printf("c%s\n", nom);
+    strcpy(newBox -> value, nom);
+    printf("c%s\n", newBox->value);
+    newBox -> next = NULL;
+    if(head[boite] == NULL) {
+        head[boite] = newBox;
     } else {
-        Box* temp=head;
+        Box* temp=head[boite];
         while(temp->next != NULL) {
             temp = temp->next;
         }
@@ -57,11 +63,43 @@ void create_element_of_box (){
     }
     
 }
-void display_element_of_box (SDL_Renderer *rend, int red, int blue, int green, int opacity, int number) {
-
-    SDL_Rect element = {espace(number)%box_height,espace(espace(number) / (box_height-height_of_element) ),height_of_element,height_of_element};
+void display_element_of_box (SDL_Renderer *rend, int red, int blue, int green, int opacity, int number, int boite[2]) {
+    int maxBox = box_height / espace(1);
+    //maxBox 
+    // printf("%-*d a\n", 20, maxBox);
+    // box_height taille carré rouge
+    // height_of_element taille carré noire
+    // espace(var) taille var * (carré noire + gap)
+    SDL_Rect element = {(espace(number)%(box_height-height_of_element+gap*3)) + boite[0],espace(espace(number) / (box_height-height_of_element) ) + boite[1],height_of_element,height_of_element};
+    //SDL_Rect element = { x, y, height_of_element, height_of_element};
     SDL_SetRenderDrawColor (rend,red,blue,green,opacity);
     SDL_RenderFillRect (rend, &element);
+}
+
+void compare_for_view () {
+    char key [1024][1024];
+    int numberKey[1024] = {0};
+    int keyIndex = 0;
+    Box *checkElem = head[0];
+    for(int nbBox = 0; checkElem != NULL; ++nbBox) {
+        int debug = 0;
+        for (int i = 0; i < keyIndex; i++)
+        {
+           if (strcmp(key[i], checkElem -> value) == 0) {
+            ++numberKey[keyIndex];
+            debug = 1;
+            }
+        }
+        
+        
+        if (debug != 1) {
+            strcpy(key[keyIndex], checkElem -> value);
+            ++keyIndex;
+        }
+        checkElem = checkElem->next;
+    }
+    
+    printf ("%s : %d\n", key[0], numberKey[0]);
 }
 
 
@@ -114,39 +152,51 @@ int main(int argc, char *argv[]) {
 
 
         //affichage des boxs
-        
-        display_element_of_box (rend,0,0,0,255,1); 
-        display_element_of_box (rend,0,0,0,255,2);
-        display_element_of_box (rend,0,0,0,255,3);  
-        display_element_of_box (rend,0,0,0,255,4);
-        display_element_of_box (rend,0,0,0,255,5);
-        display_element_of_box (rend,0,0,0,255,6);
-        display_element_of_box (rend,0,0,0,255,7);
-        display_element_of_box (rend,0,0,0,255,8);
+        Box *newElem = head[0];
+        int posBox[2] = {0, 0};
+        for(int nbBox = 0; newElem != NULL; ++nbBox) {
+            display_element_of_box (rend,0,0,0,255,nbBox, posBox);
+            newElem = newElem->next;
+        }
 
-        SDL_Rect add_button = {775, 540, 200, 75};
+        // InfoBar
+        SDL_Rect infobar = {900, 0, 300, 600};
+        SDL_SetRenderDrawColor(rend, 32, 32, 32 , 255);
+        SDL_RenderFillRect (rend, &infobar);
+
+
+
+
+        //création du add button
+        SDL_Rect add_button = {0, 540, 200, 75};
         SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
         SDL_RenderFillRect (rend, &add_button);
 
-        SDL_Rect delete_button = {650, 540, 100, 75};
+        compare_for_view();
+
+        /*SDL_Rect delete_button = {650, 540, 100, 75};
         SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
-        SDL_RenderFillRect (rend, &delete_button);
+        SDL_RenderFillRect (rend, &delete_button);*/
 
 
         
-        // TTF_Font *font = TTF_OpenFont("arial.ttf", 20);
-        // SDL_Color textColor = {255, 255, 255, 255};
-        // SDL_Surface *surface = TTF_RenderText_Solid(font, "Add Box", textColor);
-        // SDL_Texture *texture = SDL_CreateTextureFromSurface(rend, surface);
+        TTF_Font *font = TTF_OpenFont("arial.ttf", 20);
+        SDL_Color textColor = {255, 255, 255, 255};
+        SDL_Surface *surface = TTF_RenderText_Solid(font, "Add Box", textColor);
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(rend, surface);
         
 
-        // int text_width = surface->w;
-        // int text_height = surface->h;
-        // SDL_FreeSurface(surface);
+        int text_width = surface->w;
+        int text_height = surface->h;
+        SDL_FreeSurface(surface);
 
-        // SDL_Rect renderQuad = {785, 550, text_width, text_height};
-        // SDL_RenderCopy(rend, texture, NULL, &renderQuad);
-        // SDL_DestroyTexture(texture);
+        SDL_Rect recttext = { 50, 560, 100, 100};
+        SDL_RenderFillRect(rend, &recttext);
+        SDL_Rect text = recttext;
+        text.w = text_width;
+        text.h = text_height;
+        SDL_RenderCopy(rend, texture, NULL, &text);
+        SDL_DestroyTexture(texture);
 
         // SDL_Surface *surface2 = TTF_RenderText_Solid(font, "Delete Box", textColor);
         // SDL_Texture *texture2 = SDL_CreateTextureFromSurface(rend, surface2);
@@ -178,12 +228,21 @@ int main(int argc, char *argv[]) {
                 case SDL_MOUSEBUTTONDOWN:
                     if (event.button.button == SDL_BUTTON_LEFT) {
                         if (isInsideRect(event.button.x, event.button.y, add_button)) {
-                            create_element_of_box();
+                            create_element_of_box(0, "pomme");
+                            create_element_of_box(0, "banana");
                             }
                         }
-                        else if (isInsideRect(event.button.x, event.button.y, delete_button)){ 
+                        /*else if (isInsideRect(event.button.x, event.button.y, delete_button)){ 
                             
-                        }
+                        }else {
+                            for (int i = 0; i < index; i++) {
+                                if (isInsideRect(event.button.x, event.button.y, tasks[i].rect)) {
+                                    tasks[i].isDragging = true;
+                                    tasks[i].offsetX = event.button.x - tasks[i].rect.x;
+                                    tasks[i].offsetY = event.button.y - tasks[i].rect.y;
+                                }
+                            }
+                        }*/
                 break;
 
                 case SDL_QUIT:  
